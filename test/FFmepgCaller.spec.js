678c471321ae5658203a6a06abcba287e5f6b885
LP1996 <1072851826@qq.com>
2020-12-20 16:09:09 +0800

    Update FFmepgCaller.spec.js

--- test/FFmepgCaller.spec.js
+++ test/FFmepgCaller.spec.js
@@ -5,17 +5,17 @@ const ffmpeg = require('../FFmpegCaller')
 let id = 1
 
 // not reachable
-ffmpeg.run(id++, 'rtsp://admin:hik12345+@192.168.205.228:554', ['-f', 'flv', 'rtmp://192.168.205.111:1935/live1'])
+ffmpeg.run(id++, 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov', ['-f', 'flv', 'rtmp://192.168.205.111:1935/live1'])
   .then(info => console.log('convert success: ', info))
   .catch(err => console.log('convert fail: ', err))
 
 // format need to codec
-ffmpeg.run(id++, 'rtsp://admin:a1234567@192.168.20.2:554', ['-f', 'flv', 'rtmp://192.168.205.111:1935/live2'])
+ffmpeg.run(id++, 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov', ['-f', 'flv', 'rtmp://192.168.205.111:1935/live2'])
   .then(info => console.log('convert success: ', info))
   .catch(err => console.log('convert fail: ', err))
 
 // copy
-ffmpeg.run(id++, 'rtsp://admin:hik12345@192.168.205.228:554', ['-f', 'flv', 'rtmp://192.168.205.111:1935/live3'])
+ffmpeg.run(id++, 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov', ['-f', 'flv', 'rtmp://192.168.205.111:1935/live3'])
   .then(info => console.log('convert success: ', info))
   .catch(err => console.log('convert fail: ', err))
 
